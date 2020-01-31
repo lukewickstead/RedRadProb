07166810551f09d2117bebb6a1f0eed1e0fba5bf
lwickstead <luke.wickstead@mubaloo.com>
2020-01-31 13:44:14 +0000

    Adding in initial checkint of blank project for the web api

---
+++ src/server/RedRabProb/RedRabProb.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace RedRabProb.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return this.Ok("Hello");
+        }
+    }
+}
